9ab59544b7581baa53e25e0175b321d4df5c2520
jczhang02 <jczhang@live.it>
2024-05-27 20:04:38 +0800

    feat(zsh): update module

--- zsh/.config/zsh/zshrc.d/02-module.zsh
+++ zsh/.config/zsh/zshrc.d/02-module.zsh
@@ -1,11 +1,15 @@
 # ==== Module ====
-## mamba
+
+# direnv
+eval "$(direnv hook zsh)"
+
+# mamba
 eval "$(micromamba shell hook --shell zsh)"
 
-export MAMBA_EXE="/home/jc/.local/bin/micromamba";
+export MAMBA_EXE="/usr/bin/micromamba";
 export MAMBA_ROOT_PREFIX="/home/jc/.conda";
 
-__mamba_setup="$('/home/jc/.local/bin/micromamba' shell hook --shell zsh --prefix '/home/jc/.conda' 2> /dev/null)"
+__mamba_setup="$('micromamba' shell hook --shell zsh --prefix '/home/jc/.conda' 2> /dev/null)"
 
 eval "$__mamba_setup"
 
@@ -17,3 +21,5 @@ eval $(thefuck --alias)
 
 # broot
 source $XDG_CONFIG_HOME/broot/launcher/bash/br
+
+
